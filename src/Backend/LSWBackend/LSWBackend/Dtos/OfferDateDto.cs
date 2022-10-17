1d75565bb465f2d2ea15e9aa8271b94a66c1f3ca
Mo <moritzsageder@omnic-systems.com>
2022-10-17 11:06:20 +0200

    Init Backend

--- src/Backend/LSWBackend/LSWBackend/Dtos/OfferDateDto.cs
+++ src/Backend/LSWBackend/LSWBackend/Dtos/OfferDateDto.cs
@@ -1,11 +1,7 @@
 ﻿namespace Backend.Dtos
 {
-    public class TeacherDto
+    public class OfferDateDto
     {
-        public int TeacherId { get; set; }
-        public string Name { get; set; }
-		public string Email { get; set; }
-		public string Initials { get; set; }
-		public int CourseId { get; set; }
+        
     }
 }
