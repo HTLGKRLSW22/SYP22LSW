98e362d067907eaef6b8e7167b9806067709a58f
Seimouun <simon.reitmann@gmail.com>
2022-11-07 12:23:42 +0100

    frontend folder structure (#90)
    Co-authored-by: Seimouun <simon.reitmann>

---
+++ src/Frontend/src/app/feature-eager/feature-eager.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeatureEagerRoutingModule } from './feature-eager-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeatureEagerRoutingModule
+  ]
+})
+export class FeatureEagerModule { }
