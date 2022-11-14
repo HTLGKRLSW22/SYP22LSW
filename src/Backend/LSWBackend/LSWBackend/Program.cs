6d289f4f39f8caa172f1ccaba94d62da5f9976c0
MxngoDazi <83704585+MxngoDazi@users.noreply.github.com>
2022-11-14 12:48:22 +0100

    Deleted files restored (#103)

--- src/Backend/LSWBackend/LSWBackend/Program.cs
+++ src/Backend/LSWBackend/LSWBackend/Program.cs
@@ -42,10 +42,10 @@ string connectionString = builder.Configuration.GetConnectionString("LSWDb");
 string location = System.Reflection.Assembly.GetEntryAssembly()!.Location;
 string dataDirectory = Path.GetDirectoryName(location)!;
 connectionString = connectionString.Replace("|DataDirectory|", dataDirectory + Path.DirectorySeparatorChar);
-Console.WriteLine(connectionString);
 builder.Services.AddDbContext<LSWContext>(options => options.UseSqlite(connectionString));
 builder.Services.AddHostedService<DatabaseBackgroundService>();
 
+builder.Services.AddScoped<StudentsService>();
 
 #endregion
 
