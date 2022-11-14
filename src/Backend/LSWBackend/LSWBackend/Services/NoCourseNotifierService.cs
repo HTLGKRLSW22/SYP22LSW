9b76f959bf623eba2cdcf8d8769149918dae5b5c
Strasser Martin <StrasserM180084@sus.htl-grieskirchen.at>
2022-11-14 12:02:09 +0100

    Edited/Added Email-Texts

--- src/Backend/LSWBackend/LSWBackend/Services/NoCourseNotifierService.cs
+++ src/Backend/LSWBackend/LSWBackend/Services/NoCourseNotifierService.cs
@@ -1,40 +1,35 @@
-﻿namespace LSWBackend.Services
+﻿namespace LSWBackend.Services;
+
+public class NoCourseNotifierService
 {
-    public class NoCourseNotifierService
-    {
-        private readonly LSWContext _db;
-        private readonly EmailSenderService _emailSender;
+    private readonly LSWContext _db;
+    private readonly EmailSenderService _emailSender;
 
-        private DateTime enrollmentDeadline = DateTime.Now;
-        private int nofifyDaysBefore = 1;
+    private DateTime enrollmentDeadline = DateTime.Now;
+    private int nofifyDaysBefore = 1;
 
-        public NoCourseNotifierService(LSWContext db, EmailSenderService emailSender)
-        {
-            _db = db;
-            _emailSender = emailSender;
-        }
+    public NoCourseNotifierService(LSWContext db, EmailSenderService emailSender) {
+        _db = db;
+        _emailSender = emailSender;
+    }
 
-        private List<Student> GetStudentsWithoutEnrollment()
-        {
-            var dates = _db.OfferDates;
+    private List<Student> GetStudentsWithoutEnrollment() {
+        var dates = _db.OfferDates;
 
-            return _db.Students
-                 .Include(x => x.StudentOffers)
-                 .ThenInclude(x => x.Offer)
-                 .ThenInclude(x => x.OfferDates)
-                 .Where(x => x.StudentOffers.Count == 0 || !IsStudentEnrolledOnAllDays(x))
-                 .ToList();
-        }
+        return _db.Students
+             .Include(x => x.StudentOffers)
+             .ThenInclude(x => x.Offer)
+             .ThenInclude(x => x.OfferDates)
+             .Where(x => x.StudentOffers.Count == 0 || !IsStudentEnrolledOnAllDays(x))
+             .ToList();
+    }
 
-        private bool IsStudentEnrolledOnAllDays(Student student)
-        {
-            return student.StudentOffers.SelectMany(x => x.Offer.OfferDates).ToList().Count() < 3;
-        }
+    private bool IsStudentEnrolledOnAllDays(Student student) {
+        return student.StudentOffers.SelectMany(x => x.Offer.OfferDates).ToList().Count() < 3;
+    }
 
-        public void NotifyStudentsWithoutEnrollment()
-        {
-            var students = GetStudentsWithoutEnrollment();
-            students.ForEach(x => Console.WriteLine($"{x.LastName}"));
-        }
+    public void NotifyStudentsWithoutEnrollment() {
+        var students = GetStudentsWithoutEnrollment();
+        students.ForEach(x => Console.WriteLine($"{x.LastName}"));
     }
 }
