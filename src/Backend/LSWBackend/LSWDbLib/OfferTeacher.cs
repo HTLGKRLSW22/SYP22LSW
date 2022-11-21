673ece104e731433a15efb0ea1a4aac67ec18799
Lucas Kronlachner <58300254+lucaskroni@users.noreply.github.com>
2022-11-21 10:51:38 +0100

    Update OfferTeacher.cs

--- src/Backend/LSWBackend/LSWDbLib/OfferTeacher.cs
+++ src/Backend/LSWBackend/LSWDbLib/OfferTeacher.cs
@@ -5,4 +5,7 @@ public class OfferTeacher
     public int OfferTeacherId { get; set; }
     public int TeacherId { get; set; }
     public int OfferId { get; set; }
+
+    public Teacher Teacher { get; set; } = null!;
+    public Offer Offer { get; set; } = null!;
 }
