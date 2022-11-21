6e3b89e1293f686f29f62f371bf57d5a38e68f8c
Mo <moritzsageder@omnic-systems.com>
2022-11-21 12:28:51 +0100

    removed commented code

--- src/Backend/LSWBackend/LSWBackend/Controllers/StudentController.cs
+++ src/Backend/LSWBackend/LSWBackend/Controllers/StudentController.cs
@@ -33,42 +33,4 @@ public class StudentController : ControllerBase
             return BadRequest(errorMsg);
         }
     }
-
-    //private OfferService _service;
-
-    //public StudentController(OfferService service)
-    //{
-    //    this._service = service;
-    //}
-
-
-    //[HttpPost]
-    //public ActionResult DeleteStudentFromCourse(int courseId, int studentId)
-    //{
-    //    /*_service.RemoveStudentFromCourse(new StudentCourseDto
-    //    {
-    //        CourseId = courseId,
-    //        StudentId = studentId
-    //    });*/
-    //    return Ok();
-    //}
-
-    //[HttpDelete]
-    //public ActionResult<StudentDto> AddStudentToCourse(int courseId, int studentId)
-    //{
-    //    /*
-    //    var studentCourseDto = new StudentDto().CopyPropertiesFrom(_service.AddStudentToCourse(new StudentCourseDto
-    //    {
-    //        CourseId = courseId,
-    //        StudentId = studentId
-    //    }));*/
-    //    return Ok();
-
-    //}
-
-    //[HttpGet("GetStudent")]
-    //public ActionResult<StudentDto> GetStudent()
-    //{
-    //    return Ok(_service.GetStudent());
-    //}
 }
