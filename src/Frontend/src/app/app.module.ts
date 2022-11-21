7b16f4705d0685c5ab9b00750a8cf3928f97e8a0
FantasyplayHD <65398780+FantasyplayHD@users.noreply.github.com>
2022-11-21 11:28:30 +0100

    Lsw frontend admin courses (#108)
    * create Dto for Backend
    
    * Get data for admin/courses-list from backend
    
    Co-authored-by: erikson835 <Neulinger.Erik@sus.htl-grieskirchen.at>

--- src/Frontend/src/app/app.module.ts
+++ src/Frontend/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiModule, BASE_PATH } from './swagger';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeatureEagerModule } from './feature-eager/feature-eager.module';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -11,10 +14,14 @@ import { FeatureEagerModule } from './feature-eager/feature-eager.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    ApiModule,
     FeatureEagerModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    {provide: BASE_PATH, useValue: environment.apiRoot}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
