9e757d111178903ac09f9234eeddd858468aa0f8
erikson835 <84027632+erikson835@users.noreply.github.com>
2022-11-21 08:58:14 +0100

    create Dto for Backend (#106)

--- src/Frontend/src/app/app-routing.module.ts
+++ src/Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./feature-lazy/feature-lazy.module').then(x => x.FeatureLazyModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
