18c1fd4ab5606ffc8a402e1d26b89058299f9614
Bruno Hasibeder <HasibederB180106@sus.htl-grieskirchen.at>
2022-11-21 12:48:26 +0100

    fix courses-list to work properly I guess

--- src/Frontend/src/app/admin/courses-list/courses-list.component.ts
+++ src/Frontend/src/app/admin/courses-list/courses-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {OfferDate, OfferDto, OffersService, ReplyDTO} from "../../swagger";
 
 @Component({
   selector: 'app-courses-list',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesListComponent implements OnInit {
 
-  constructor() { }
+  allCourses: OfferDto[] = [];
+
+  constructor(private offersService: OffersService) {}
 
   ngOnInit(): void {
+    console.log('admin::courses-list - Courses List works');
+    this.offersService.offersGetOffersGet()
+      .subscribe((x: OfferDto[])=>{
+        this.allCourses = x;
+        console.table(x);
+      });
   }
 
+  deleteCourse(offerId: number): void {
+    console.log(`admin::courses-list::deleteCourse - Button Delete clicked - offerId:${offerId}`);
+    this.offersService.offersDeleteOfferDelete(offerId)
+      .subscribe((x: ReplyDTO)=>{
+        console.table(x);
+        this.reloadField();
+      });
+  }
+
+  editCourse(offerId: number): void {
+    console.log(`admin::courses-list::editCourse - Button Edit clicked - offerId:${offerId}`);
+    this.reloadField();
+  }
+
+  dateConverter(offerdates: OfferDate[]): string{
+    if(offerdates.length === 0){
+      return '';
+    }
+    if(offerdates[0]!==offerdates[offerdates.length-1]){
+      return `${offerdates[0].startDate} - ${offerdates[offerdates.length-1].startDate}`;
+    }
+    return `${offerdates[0].startDate}`;
+  }
+
+  reloadField() : void{
+    this.offersService.offersGetOffersGet()
+      .subscribe((x: OfferDto[])=>{
+        this.allCourses = x;
+        console.table(x);
+      });
+  }
 }
