8ceb1592a9a6f9190d88c92a2638250847a2537d
mbrunmaier18 <brunmaiermax@yahoo.com>
2022-11-21 12:27:49 +0100

    improved Freistellungen
    The list for the Offers in FreistllungsDto was changed to a single Offer and the Service was revised

---
+++ src/Backend/LSWBackend/LSWBackend/Services/FreistellungsService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using LSWBackend.Dtos;
+
+namespace LSWBackend.Services;
+
+public class FreistellungsService
+{
+
+    private readonly LSWContext _db;
+    public FreistellungsService(LSWContext db) => _db = db;
+
+    public bool SetFreistellung(FreistellungsDto freistellungsDto) {
+        Student? student = _db.Students.SingleOrDefault(x => x.StudentId == freistellungsDto.StudentId);
+        Offer? freistellung = _db.Offers.SingleOrDefault(x => x.OfferId == freistellungsDto.FreistellungsId);
+        if (freistellung != null && freistellung.VisibleForStudents == 0) {
+            if (student != null && student.StudentOffers.Count == 0) {
+
+                _db.StudentOffers.Add(new StudentOffer {
+                    OfferId = freistellung.OfferId,
+                    StudentId = student.StudentId,
+                });
+                _db.SaveChanges();
+                return true;
+            }
+            else if (student != null) {
+                foreach (StudentOffer offer in student.StudentOffers) {
+                    foreach (OfferDate date in offer.Offer.OfferDates) {
+                        if (date.StartDate.Date == freistellung.OfferDates[0].StartDate.Date) {
+                            _db.StudentOffers.Remove(offer);
+                            _db.SaveChanges();
+                        }
+                    }
+                }
+                _db.StudentOffers.Add(new StudentOffer {
+                    OfferId = freistellung.OfferId,
+                    StudentId = student.StudentId,
+                });
+                _db.SaveChanges();
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
