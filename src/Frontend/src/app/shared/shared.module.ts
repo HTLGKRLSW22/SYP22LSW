556830440e2c3081fea3ccc4c78633cc01d98427
Seimouun <simon.reitmann>
2022-11-21 12:02:00 +0100

    Sidebar, Fonts,SVGs

--- src/Frontend/src/app/shared/shared.module.ts
+++ src/Frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { HtlLogoComponent } from './htl-logo/htl-logo.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideBarComponent,
+    HtlLogoComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SideBarComponent
   ]
 })
 export class SharedModule { }
