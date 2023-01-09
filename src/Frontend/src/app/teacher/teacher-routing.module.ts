7da062102954bad60385d5610dd8db979589c0b7
jthann18 <Thanner.Julian@sus.htl-grieskirchen.at>
2023-01-09 12:50:21 +0100

    Added TeacherView Component in combination with the Course Component, Added empty Components with routing

--- src/Frontend/src/app/teacher/teacher-routing.module.ts
+++ src/Frontend/src/app/teacher/teacher-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeacherViewComponent } from './teacher-view/teacher-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'view', component: TeacherViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
